cf4761bfc5e06c7a1ed3feaef0962f69489752fb
artyomtimofeev <tema.timofeev20128@gmail.com>
2021-05-03 23:54:40 +0300

    added header

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,10 +4,12 @@ class UserController {
     async createUser(req, res) {
         const {name, surname} = req.body
         const newPerson = await db.query('INSERT INTO person (name, surname) values ($1, $2) RETURNING *', [name, surname])
+        res.set({'Access-Control-Allow-Origin': '*'})
         res.json(newPerson.rows[0])
     }
     async getUsers(req, res) {
         const users = await db.query('SELECT * FROM person')
+        res.set({'Access-Control-Allow-Origin': '*'})
         res.json(users.rows)
     }
     async getOneUser(req, res) {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 80
 
 const app = express()
 
