e2edfcfd85a03bc1be36cc6a562b6824b8800522
artyomtimofeev <tema.timofeev20128@gmail.com>
2021-05-04 20:12:55 +0300

    added Access-Control-Allow-Origin:* in OPTIONS

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
 
-const https = require( "https" );
+const https = require("https");
 const http = require('http');
 const fs = require( "fs" );
+const cors = require('cors');
 
 const privateKey  = fs.readFileSync('sslcert/private.key', 'utf8');
 const certificate = fs.readFileSync('sslcert/certificate.crt', 'utf8');
 
 const credentials = {key: privateKey, cert: certificate};
 
-const PORT = process.env.PORT || 80
-
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 app.use('/api', userRouter)
-
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
